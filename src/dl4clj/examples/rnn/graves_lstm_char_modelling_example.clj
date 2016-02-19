b0754ca4966fb5475bbf969966599827c25bae3f
Joachim De Beule <joachim.de.beule@gmail.com>
2016-02-19 15:42:45 +0100

    reimplement according to original

--- src/dl4clj/examples/rnn/graves_lstm_char_modelling_example.clj
+++ src/dl4clj/examples/rnn/graves_lstm_char_modelling_example.clj
@@ -9,7 +9,8 @@ For general instructions using deeplearning4j's implementation of recurrent neur
   dl4clj.examples.rnn.graves-lstm-char-modelling-example
   (:require [dl4clj.examples.example-utils :refer (shakespeare)]
             [dl4clj.examples.rnn.lr-character-iterator :refer (lr-character-iterator)]
-            ;; [dl4clj.examples.example-utils :refer (+default-character-set+)]
+            [dl4clj.examples.rnn.character-iterator :refer (get-shakespeare-iterator)]
+            [nd4j.linalg.dataset.api.iterator.data-set-iterator :refer (input-columns total-outcomes)]
             [dl4clj.nn.conf.layers.graves-lstm]
             [dl4clj.nn.conf.layers.rnn-output-layer]
             [nd4clj.linalg.factory.nd4j :refer (zeros set-enforce-numerical-stability!)]
@@ -20,97 +21,132 @@ For general instructions using deeplearning4j's implementation of recurrent neur
             [dl4clj.nn.multilayer.multi-layer-network :refer (multi-layer-network init get-layers get-layer rnn-clear-previous-state rnn-time-step)]
             [dl4clj.nn.api.model :as model]
             [dl4clj.nn.api.classifier :as classifier]
-            [dl4clj.nn.api.layer]))
+            [dl4clj.nn.api.layer])
+  (:import [java.util Random]))
 
-(defn graves-lstm-char-modeling-net [iter hidden-layer-size]
-  (let [opts {:optimization-algo :stochastic-gradient-descent
-              :iterations 1
-              :learning-rate 0.1
-              :rms-decay 0.95
-              :seed 12345
-              :regularization true
-              :l2 0.001
-              :list 3
-              :layers {0 {:graves-lstm
-                          {:n-in (count (:valid-chars iter))
-                           :n-out hidden-layer-size
-                           :updater :rmsprop
-                           :activation :tanh
-                           :weight-init :distribution
-                           :dist {:normal {:mean 0.0, :std 0.01}}}}
-                       1 {:graves-lstm
-                          {:n-in hidden-layer-size
-                           :n-out hidden-layer-size
-                           :updater :rmsprop
-                           :activation :tanh
-                           :weight-init :distribution
-                           :dist {:normal {:mean 0.0, :std 0.01}}}}
-                       2 {:rnnoutput
-                          {:loss-function :mcxent
-                           :n-in hidden-layer-size
-                           :n-out (count (:valid-chars iter))
-                           :updater :rmsprop
-                           :activation :softmax
-                           :weight-init :distribution
-                           :dist {:normal {:mean 0.0, :std 0.01}}}}}
-              :pretrain false
-              :backprop true}
-        net (multi-layer-network (neural-net-configuration opts))]
-    (init net)
-    (dotimes [i (count (get-layers net))]
-      (println "Number of parameters in layer "  i  ": "  (model/num-params (get-layer net i))))
-    (println "Total number of network parameters: " (reduce + (map model/num-params (get-layers net))))
-    net))
+
+(def mini-batch-size 32)
+(def example-length 100)
+(def examples-per-epoch (* mini-batch-size 50))
+(def lstm-layer-size 200)
+(def num-epochs 30)
+(def generation-initialization nil)
+(def rng (Random. 12345))
+(def n-characters-to-sample 300)
+(def n-samples-to-generate 4)
+
+;; Get a DataSetIterator that handles vectorization of text into something we can use to train our
+;; GravesLSTM network.
+(def iter (get-shakespeare-iterator mini-batch-size example-length examples-per-epoch))
+
+;; Set up network configuration:
+(def conf (neural-net-configuration
+           {:optimization-algo :stochastic-gradient-descent
+            :iterations 1
+            :learning-rate 0.1
+            :rms-decay 0.95
+            :seed 12345
+            :regularization true
+            :l2 0.001
+            :list 3
+            :layers {0 {:graves-lstm
+                        {:n-in (input-columns iter)
+                         :n-out lstm-layer-size
+                         :updater :rmsprop
+                         :activation :tanh
+                         :weight-init :distribution
+                         :dist {:uniform {:lower -0.08, :upper 0.08}}}}
+                     1 {:graves-lstm
+                        {:n-in lstm-layer-size
+                         :n-out lstm-layer-size
+                         :updater :rmsprop
+                         :activation :tanh
+                         :weight-init :distribution
+                         :dist {:uniform {:lower -0.08, :upper 0.08}}}}
+                     2 {:rnnoutput
+                        {:loss-function :mcxent
+                         :activation :softmax
+                         :updater :rmsprop
+                         :n-in lstm-layer-size
+                         :n-out (total-outcomes iter)
+                         :weight-init :distribution
+                         :dist {:uniform {:lower -0.08, :upper 0.08}}}}}
+            :pretrain false
+            :backprop true}))
+(def net (multi-layer-network conf))
+(init net)
+;; not yet implemented:
+;; net.setListeners(new ScoreIterationListener(1));
+
+;; Print the  number of parameters in the network (and for each layer)
+(dotimes [i (count (get-layers net))]
+  (println "Number of parameters in layer "  i  ": "  (model/num-params (get-layer net i))))
+(println "Total number of network parameters: " (reduce + (map model/num-params (get-layers net))))
+
+;; Do training, and then generate and print samples from network
 
 (defn- sample-from-distribution 
   "Sample from a probability distribution over discrete classes given as a vector of probabilities
   summing to 1.0."
-  [distribution] 
-  (let [toss (rand)]
+  [distribution ^Random rng] 
+  (let [d (.nextDouble rng)]
     (loop [i 0
            sum (nth distribution 0)]
-      (cond (<= toss sum) i
+      (cond (<= d sum) i
             (< i (count distribution)) (recur (inc i) (+ sum (nth distribution i)))
-            :else (throw (IllegalArgumentException. (str "Distribution is invalid? toss= " toss ", sum=" sum)))))))
+            :else (throw (IllegalArgumentException. (str "Distribution is invalid? d= " d ", sum=" sum)))))))
 
-(defn sample [net iter start-string num-samples sample-length]
-  (let [char-to-idx (:char-to-idx iter)
-        idx-to-char (zipmap (vals char-to-idx) (keys char-to-idx))
-        initialization-input (zeros [num-samples (count char-to-idx) (count start-string)])]    
-    ;; Fill input with data from start-string
-    (dotimes [i (count start-string)]
-      (let [idx (char-to-idx (nth start-string i))]
+(defn sample-characters-from-network [initialization  net iter rng characters-to-sample num-samples]
+  ;; Set up initialization. If no initialization: use a random character
+  (let [;; set up initialization. If no initialization: use a random character.
+        initialization (or initialization (str (rand-nth (seq (:valid-characters iter)))))
+        char-to-idx-map (:char-to-idx-map iter)
+        idx-to-char-map (zipmap (vals char-to-idx-map) (keys char-to-idx-map))
+        initialization-input (zeros [num-samples (input-columns iter) (count initialization)])]
+    ;; create input for initialization
+    (dotimes [i (count initialization)]
+      (let [idx (char-to-idx-map (nth initialization i))]
         (dotimes [j num-samples]
           (put-scalar initialization-input [j idx i] 1.0))))
-    
+    ;; Sample from network (and feed samples back into input) one character at a time (for all
+    ;; samples). Sampling is done in parallel here
     (rnn-clear-previous-state net)
+    (let [output (rnn-time-step net initialization-input)
+          sb (for [i (range num-samples)] (StringBuilder.))]
+      (loop [output (tensor-along-dimension output (- (.size output 2) 1) [1 0]) ;; (dec (count start-string))
+             i 0]
+        ;; set up next input (single time step) by sampling from previous output
+        (let [next-input (zeros [num-samples (input-columns iter)])]
+          (dotimes [s num-samples]
+            (let [output-prob-distribution (double-array (total-outcomes iter))]
+              (dotimes [j (count output-prob-distribution)]
+                (aset output-prob-distribution j (get-double output [s j])))
+              (let [sampled-character-idx (sample-from-distribution output-prob-distribution rng)]
+                ;; prepare next time step input
+                (put-scalar next-input [s sampled-character-idx] 1.0)
+                ;; add sampled character to stringbuilder (human readable output)
+                (.append (nth sb s) (idx-to-char-map sampled-character-idx)))))
+          (when (< i characters-to-sample)
+            ;; do one time step of forward pass
+            (recur (rnn-time-step net next-input)
+                   (inc i)))))
+    (map #(.toString %) sb))))
 
-    (loop [samples (for [i (range num-samples)] start-string)
-           output (tensor-along-dimension (rnn-time-step net initialization-input)
-                                          (dec (count start-string))
-                                          [1 0])]
-      ;; Set up next input (single time step) by sampling from previous output
-      (let [next-input (zeros num-samples (count char-to-idx))
-            next-samples (for [s (range num-samples)]
-                           (let [sampled-character-idx (sample-from-distribution (data (slice output s 1)))]
-                             (put-scalar next-input [s sampled-character-idx] 1.0)
-                             (str (nth samples s)  (idx-to-char sampled-character-idx))))]
-        (if (< (count (first samples)) sample-length)
-          (recur next-samples (rnn-time-step net next-input))
-          next-samples)))))
-
-(comment
+;; (def i 0)
+(dotimes [i num-epochs]
+  (classifier/fit net iter)
   
-  ;; custom dataset iterator:
-  (def iter (lr-character-iterator (shakespeare)))
+  (println "--------------------")
+  (println "Completed epoch " i )
+  (println "Sampling characters from network given initialization \"\"")
+  (loop [j 0
+         (def samples (sample-characters-from-network generation-initialization net iter rng n-characters-to-sample n-samples-to-generate))]
+    (println "----- Sample " j " -----")
+    (println (first samples))
+    (println)
+    (when-not (empty? samples)
+      (recur (inc j) (rest samples))))
 
-  (def net (graves-lstm-char-modeling-net iter 100))
+  ;; Reset iterator for another epoch
+  (reset iter))
 
-  (dotimes [i 10]
-    (.reset iter)
-    (classifier/fit net iter)
-    (println "finished iteration" i)
-    (doseq [s (sample net iter "a" 10 80)]
-      (println "sample:" s)))
-
-  )
