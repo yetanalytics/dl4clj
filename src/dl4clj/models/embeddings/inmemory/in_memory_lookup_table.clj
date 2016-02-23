1b51f21cd1023225ae406be5f15d14b633f39e66
Joachim De Beule <joachim.de.beule@gmail.com>
2016-02-23 01:06:37 +0100

    add get-vocab

--- src/dl4clj/models/embeddings/inmemory/in_memory_lookup_table.clj
+++ src/dl4clj/models/embeddings/inmemory/in_memory_lookup_table.clj
@@ -9,3 +9,5 @@
   ([vocab-cache vector-length use-ada-grad? learning-rate rng negative]
    (InMemoryLookupTable. vocab-cache (int vector-length) (boolean use-ada-grad?) (double learning-rate) rng (double negative))))
   
+(defn get-vocab [^InMemoryLookupTable this]
+  (.getVocab this))
