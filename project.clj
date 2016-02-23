bdbacca98310a823e26d077bba315498e4638c2e
Joachim De Beule <joachim.de.beule@gmail.com>
2016-02-23 01:05:45 +0100

    update

--- project.clj
+++ project.clj
@@ -6,7 +6,8 @@
                  [org.deeplearning4j/deeplearning4j-nlp "0.4-rc3.8"]
                  [org.apache.commons/commons-io "1.3.2"]
                  [org.nd4j/nd4j-jblas "0.4-rc3.6"]            ;; MacBook requirement
-                 [org.nd4j/nd4j-x86 "0.4-rc3.5"]              ;; CPU                 
+                 [org.nd4j/nd4j-x86 "0.4-rc3.5"]              ;; CPU
+                 [org.nd4j/canova-api "0.0.0.14"]
                  [org.clojure/data.json "0.2.6"]
                  ;; [com.google.collections/google-collections "1.0"]
                  ]) 
