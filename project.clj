3105f6bbdfaba54dbf59d93e225cec0e3ff5566c
Joachim De Beule <joachim.de.beule@gmail.com>
2016-04-29 15:14:55 +0200

    prep for clojars release

--- project.clj
+++ project.clj
@@ -1,5 +1,17 @@
+;; Copyright (c) 2014 Engagor
+;;
+;; The use and distribution terms for this software are covered by the
+;; BSD License (http://opensource.org/licenses/BSD-2-Clause)
+;; which can be found in the file LICENSE at the root of this distribution.
+;; By using this software in any fashion, you are agreeing to be bound by
+;; the terms of this license.
+;; You must not remove this notice, or any other, from this software.
+
 (defproject dl4clj "0.0.1-SNAPSHOT"
   :description "ports of some DL4J examples to Clojure"
+  :url "https://github.com/engagor/dl4clj"
+  :license {:name "BSD C2"
+            :url "http://opensource.org/licenses/BSD-2-Clause"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.deeplearning4j/deeplearning4j-core "0.4-rc3.7"]
                  [org.deeplearning4j/deeplearning4j-ui "0.4-rc3.8"]
