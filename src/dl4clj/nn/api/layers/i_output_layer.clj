921445644737e4217079fb8afe98bac6d20f13d0
FeLungs <will@yetanalytics.com>
2017-04-25 14:27:41 -0400

    updated the i-output-layer ns which contains the methods defined in the IoutputLayer interface of dl4j

--- src/dl4clj/nn/api/layers/i_output_layer.clj
+++ src/dl4clj/nn/api/layers/i_output_layer.clj
@@ -1,25 +1,39 @@
-(ns dl4clj.nn.api.layers.i-output-layer
-  (:import [org.deeplearning4j.nn.api.layers IOutputLayer])
-  (:require [dl4clj.nn.api.layer :refer :all]
-            [dl4clj.nn.api.classifier :refer :all]
-            [dl4clj.nn.api.model :refer :all]))
+(ns ^{:doc "Implementation of the methods found in the IOutputLayer Interface.
+fns are for output layers (those that calculate gradients with respect to a labels array)
+see: https://deeplearning4j.org/doc/org/deeplearning4j/nn/api/layers/IOutputLayer.html"}
+    dl4clj.nn.api.layers.i-output-layer
+  (:import [org.deeplearning4j.nn.api.layers IOutputLayer]))
 
 (defn compute-score
-  "Compute score after labels and input have been set."
-  [& {:keys [this full-network-l1 full-network-l2 training?]}]
-  (.computeScore this full-network-l1 full-network-l2 training?))
+  "Compute score after labels and input have been set.
+
+  output-layer is the output layer in question
+  full-network-l1 (double) is the l1 regularization term for the model the layer is apart of
+  full-network-l2 (double) is the l2 regularization term for the model the layer is aprt of
+   -note: it is okay for L1 and L2 to be set to 0.0 if regularization was not used
+  training? (boolean) are we traing the model or testing it?"
+  [& {:keys [output-layer full-network-l1 full-network-l2 training?]}]
+  (.computeScore output-layer full-network-l1 full-network-l2 training?))
 
 (defn compute-score-for-examples
-  "Compute the score for each example individually, after labels and input have been set."
-  [& {:keys [this full-network-l1 full-network-l2]}]
-  (.computeScoreForExamples this full-network-l1 full-network-l2))
+  "Compute the score for each example individually, after labels and input have been set.
+
+  output-layer is the output layer in question
+  full-network-l1 (double) is the l1 regularization term for the model the layer is apart of
+  full-network-l2 (double) is the l2 regularization term for the model the layer is aprt of
+   -note: it is okay for L1 and L2 to be set to 0.0 if regularization was not used"
+  [& {:keys [output-layer full-network-l1 full-network-l2]}]
+  (.computeScoreForExamples output-layer full-network-l1 full-network-l2))
 
 (defn get-labels
-  "Get the labels array previously set with setLabels(INDArray)"
-  [this]
-  (.getLabels this))
+  "Get the labels array previously set with set-labels!"
+  [output-layer]
+  (.getLabels output-layer))
 
-(defn set-labels
-  "Set the labels array for this output layer"
-  [& {:keys [this labels]}]
-  (.setLabels this labels))
+(defn set-labels!
+  "Set the labels array for this output layer and returns the layer
+
+  labels is an INDArray of labels"
+  [& {:keys [output-layer labels]}]
+  (doto output-layer
+    (.setLabels labels)))
