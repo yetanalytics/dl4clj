9d6880f9d08dbfe4111332b6ade2a976eb57cc08
FeLungs <will@yetanalytics.com>
2017-04-26 17:39:03 -0400

    started writing type-checking multimethod dsl

--- src/dl4clj/utils.clj
+++ src/dl4clj/utils.clj
@@ -1,5 +1,13 @@
 (ns dl4clj.utils
-  (:import [org.deeplearning4j.nn.api Model Layer]))
+  (:import [org.deeplearning4j.nn.api Model Layer]
+           [org.nd4j.linalg.api.ndarray INDArray]
+           [org.deeplearning4j.datasets DataSets]
+           [org.deeplearning4j.datasets.datavec
+            RecordReaderDataSetIterator
+            RecordReaderMultiDataSetIterator$Builder
+            RecordReaderMultiDataSetIterator
+            SequenceRecordReaderDataSetIterator
+            SequenceRecordReaderDataSetIterator$AlignmentMode]))
 
 ;; move contains-many? here and change all of the breaking changes that ensue
 
@@ -33,11 +41,40 @@
 (defn indexed [col]
   (map vector col (range)))
 
-;; make this a multi method
-(defn type-checking
-  [t]
-  t)
-;; INDArray
+(defn typez
+  [opts]
+  (first (keys opts)))
+
+(defmulti type-checking
+  "ensures types are correct before being passed to java methods.  If they are not, throws"
+  typez)
+
+(defmethod type-checking :INDArray [opts]
+  (let [data (:INDArray opts)]
+    (if (= org.nd4j.linalg.cpu.nativecpu.NDArray (type data))
+      ;; or .gup.NDArray
+      data
+      (assert false "incorrect input type, should be an NDArray"))))
+
+(defmethod type-checking :string [opts]
+  (let [data (:string opts)]
+    (if (= java.lang.String (type data))
+      data
+      (assert false "incorrect input type, should be a java.lang.String"))))
+
+(defmethod type-checking :dataset [opts]
+  (let [data (:dataset opts)]
+    (if (= org.nd4j.linalg.dataset.DataSet (type data))
+      data
+      (assert false "incorrect input type, should be a DataSet"))))
+
+(defmethod type-checking :alignment-mode [opts]
+  (let [data (:alignment-mode opts)]
+    (if (= org.deeplearning4j.datasets.datavec.SequenceRecordReaderDataSetIterator$AlignmentMode
+           (type data))
+      data
+      (assert false "incorrect input type, should be an AlignmentMode enum"))))
+
 ;; Layer
 ;; gradient
 ;; nn's
@@ -49,7 +86,6 @@
 ;; array
 ;; collection
 ;; all the layer types
-;; dataset
 ;; dataset Iterator
 ;; Updater
 ;; Tree
