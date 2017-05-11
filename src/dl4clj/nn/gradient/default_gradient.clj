95f9531c048b6aa1ce93421765105ce329c79898
FeLungs <will@yetanalytics.com>
2017-05-11 12:30:04 -0400

    updated default gradient ns

--- src/dl4clj/nn/gradient/default_gradient.clj
+++ src/dl4clj/nn/gradient/default_gradient.clj
@@ -1,16 +1,19 @@
-(ns dl4clj.nn.gradient.default-gradient
+(ns ^{:doc "Default gradient implementation. Basically lookup table for ndarrays
+see: https://deeplearning4j.org/doc/org/deeplearning4j/nn/gradient/DefaultGradient.html"}
+    dl4clj.nn.gradient.default-gradient
   (:import [org.deeplearning4j.nn.gradient DefaultGradient]))
 
 (defn constructor
-  ([]
-   (DefaultGradient.))
-  ([flattened-gradient]
-   (DefaultGradient. flattened-gradient)))
+  [& {:keys [flattened-gradient]
+      :as opts}]
+  (if (contains? opts :flattened-gradient)
+    (DefaultGradient. flattened-gradient)
+    (DefaultGradient.)))
 
-(defn clear
+(defn clear!
   "Clear residual parameters (useful for returning a gradient and then clearing old objects)"
   [this]
-  (.clear this))
+  (doto this (.clear)))
 
 (defn flattening-order-for-variables
   "Return the gradient flattening order for the specified variable, or null if it is not explicitly set"
@@ -34,13 +37,9 @@
   [this]
   (.gradientForVariable this))
 
-(defn set-gradient-for
+(defn set-gradient-for!
   "Update gradient for the given variable; also (optionally) specify the order in which the array should be flattened to a row vector"
   [& {:keys [this variable new-gradient flattening-order]}]
   (if flattening-order
     (.setGradientFor this variable new-gradient flattening-order)
     (.setGradientFor this variable new-gradient)))
-
-(defn to-string
-  [this]
-  (.toString this))
