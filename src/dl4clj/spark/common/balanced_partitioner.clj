94a5316a0d8e54766613b1ad4025e206b076181e
FeLungs <will@yetanalytics.com>
2017-06-15 13:33:19 -0400

    implemented the creation of a balanced partitioner
    [#136364031]

---
+++ src/dl4clj/spark/common/balanced_partitioner.clj
@@ -0,0 +1,30 @@
+(ns ^{:doc "see: https://deeplearning4j.org/doc/org/deeplearning4j/spark/impl/common/repartition/BalancedPartitioner.html"}
+    dl4clj.spark.common.balanced-partitioner
+  (:import [org.deeplearning4j.spark.impl.common.repartition BalancedPartitioner]))
+
+(defn new-balanced-partitioner
+  "This is a custom partitioner which attempts to keep contiguous elements
+  (i.e., those elements originally in the same partition) together much more frequently.
+  Furthermore, it is less prone to producing larger or smaller than
+  expected partitions, as it is entirely deterministic
+
+  :n-partitions (int), desired number of partitions
+
+  :elements-per-partition (int), the number of elements within each partition
+
+  :remainder (int), expected number of stray elements"
+  [& {:keys [n-partitions elements-per-partition remainder]}]
+  (BalancedPartitioner. n-partitions elements-per-partition remainder))
+
+(defn get-partition-by-key
+  "returns the partition int identifier by the partitions key
+
+  :partition-key (object), the key which identifies a partition
+   - I think, not sure, will need to test"
+  [& {:keys [balanced-partioner partition-key]}]
+  (.getPartition balanced-partioner partition-key))
+
+(defn get-number-of-partitions
+  "return the number of partitions in a balanced partitioner"
+  [balanced-partitioner]
+  (.numPartitions balanced-partitioner))
