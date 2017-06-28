3770cd26ae30597790e43054b522c7a8f3bfdcb1
FeLungs <will@yetanalytics.com>
2017-06-28 16:06:01 -0400

    figured out (to a degreee) the dl4j serve route builder class
    [#136364031

--- src/dl4clj/streaming/routes/dl4j_serve_route_builder.clj
+++ src/dl4clj/streaming/routes/dl4j_serve_route_builder.clj
@@ -1,14 +1,49 @@
 (ns dl4clj.streaming.routes.dl4j-serve-route-builder
-  (:import [org.deeplearning4j.streaming.routes DL4jServeRouteBuilder]))
+  (:import [org.deeplearning4j.streaming.routes DL4jServeRouteBuilder]
+           [org.apache.camel.processor DynamicRouter]
+           [org.apache.camel.impl DefaultCamelContext]
+           [org.apache.camel Processor]))
 
-;; it looks like i need to call the camel methods for setting up the builder?
-;; no idea, cant call constructor...
-;; maybe I dont have the camel dep?
-;; will need to look at the test classes they set up
+(defn new-dl4j-serve-route-builder
+  ":model-uri (str), uri for a saved multi layer network or computational graph
 
-#_(defn new-dl4j-serve-route-builder
-  "Serve results from a kafka queue.
-  The input to the route can either be a pre serialized ndarray
-  or a normal ndarray itself."
-  []
-  (DL4jServeRouteBuilder.))
+   :kafka-broker (str), not sure, need to learn more about kafka
+
+   :consuming-topic (str), not sure, need to learn more about kafka
+
+   :comp-graph? (boolean), are we loading a computation graph?
+
+   :output-uri (str), not sure, need to look more into kafka
+    - looks like the location to route the result of running data through the supplied model
+
+   :final-processor (Camel Processor), believe performns the processing/routing of the request?
+
+   :group-id (str), the group id for a kafka stream?, defaults to 'dl4j-serving'
+
+   :zoo-keeper-host (str), the path to the zoo keeper host, defaults to 'localhost'
+
+   :zoo-keeper-port (int), the port zoo keeper lives on, defaults to 2181
+
+   :before-processor (Camel Processor), has some interaction with the processing/routing
+    - defaults to nil, as this creates a new processor in the dl4j java src
+
+   :configure? (boolean), do you want to configure the route builder or just return
+    the object itself without the configure call made"
+  [& {:keys [model-uri kafka-broker consuming-topic
+             comp-graph? output-uri final-processor
+             group-id zoo-keeper-host zoo-keeper-port
+             before-processor configure?]
+      :or {comp-graph? false
+           final-processor (DynamicRouter. (DefaultCamelContext.))
+           before-processor nil
+           zoo-keeper-host "localhost"
+           zoo-keeper-port 2181
+           group-id "dl4j-serving"
+           configure? true}}]
+  (let [route-builder (DL4jServeRouteBuilder. model-uri kafka-broker consuming-topic
+                                              comp-graph? output-uri final-processor
+                                              group-id zoo-keeper-host zoo-keeper-port before-processor)]
+    (if configure?
+      (doto  route-builder
+        .configure)
+      route-builder)))
