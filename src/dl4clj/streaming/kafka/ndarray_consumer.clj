cafdc367266d48d2a5fa60af3e1e661fee642e2d
FeLungs <will@yetanalytics.com>
2017-06-28 12:15:55 -0400

    fixed the ndarray consumer constructor.  Need to test to verify it works with kafka
    [#136364031]

--- src/dl4clj/streaming/kafka/ndarray_consumer.clj
+++ src/dl4clj/streaming/kafka/ndarray_consumer.clj
@@ -1,12 +1,25 @@
 (ns ^{:doc "see: https://deeplearning4j.org/doc/org/deeplearning4j/streaming/kafka/NDArrayConsumer.html"}
     dl4clj.streaming.kafka.ndarray-consumer
-  (:import [org.deeplearning4j.streaming.kafka NDArrayConsumer]))
+  (:import [org.deeplearning4j.streaming.kafka NDArrayConsumer]
+           [org.apache.camel.impl DefaultCamelContext]
+           [org.apache.camel.impl DefaultConsumerTemplate]))
 
-#_(defn new-ndarray-consumer
-  "NDArray consumer for receiving ndarrays off of kafka"
-  []
-  (NDArrayConsumer.))
-;; more constructor issues
+(defn new-ndarray-consumer
+  "NDArray consumer for receiving ndarrays off of kafka
+
+  :topic-name (str), the name of the kafka topic
+
+  :kafka-uri (str), the uri to a kafka stream containing NDArrays"
+  [& {:keys [topic-name kafka-uri]}]
+  (let [camel-context (DefaultCamelContext.)
+        consumer-template (DefaultConsumerTemplate. camel-context)]
+   (NDArrayConsumer. camel-context consumer-template topic-name
+                     kafka-uri false)))
+
+(defn start-consumer!
+  "starts the consumer and returns it"
+  [consumer]
+  (.start consumer))
 
 (defn get-arrays-from-stream
   "Receive an ndarray of arrays from the queue"
@@ -18,8 +31,3 @@
    - need to test if this gets everything as a single array or a single array form the que"
   [consumer]
   (.getINDArray consumer))
-
-(defn start-consumer!
-  "starts the consumer and returns it"
-  [consumer]
-  (.start consumer))
